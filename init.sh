18aec82b154ce81157557a9d33848d5d3faa9a7a
Fangjun Kuang <csukuangfj@gmail.com>
2018-02-17 16:58:53 +0800

    fix init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,9 @@ rm -v ~/.vimrc
 
 ln  -sfv $PWD/.vimrc ~/
 ln -sfv $PWD/.vim ~/
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 
 # Then go to ~/.vim/
 # vim kfj-bundle
