1376092d4a15944bb7dc04e8bac008a1d25ebc5f
Kuang Fangjun <csukuangfj@gmail.com>
2018-06-05 17:45:54 +0800

    add jupyter notebook key binding

--- init.sh
+++ init.sh
@@ -7,6 +7,11 @@ rm -v ~/.ideavimrc
 ln -sfv $PWD/.vimrc ~/
 ln -sfv $PWD/.ideavimrc ~/
 ln -sfv $PWD/.vim ~/
+
+mkdir -p $HOME/.jupyter/custom
+ln -sfv $PWD/jupyter/custom.js ~/.jupyter/custom/
+
+
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
