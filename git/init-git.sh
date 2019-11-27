bf1e7dbd1f07b0c6c285e43d26dbea7f4caffe7e
Fangjun Kuang <csukuangfj@gmail.com>
2019-11-27 16:23:46 +0800

    fix an error in init git

--- git/init-git.sh
+++ git/init-git.sh
@@ -3,5 +3,5 @@
 rm -fv ~/.gitconfig
 rm -fv ~/.gitignore_global
 
-ln -s $PWD/git/gitconfig ~/.gitconfig
-ln -s $PWD/git/gitignore_global ~/.gitignore_global
+ln -s $PWD/gitconfig ~/.gitconfig
+ln -s $PWD/gitignore_global ~/.gitignore_global
