6e0752057a1332e53319f629f5d801644ea90232
Fangjun Kuang <fangjun.kuang@mobvoi.com>
2020-02-05 13:30:17 +0800

    add git hooks for yapf and isort.

---
+++ git/hooks/README.md
@@ -0,0 +1,13 @@
+
+
+# Python
+
+```
+pip install yapf==0.27.0
+pip install isort==4.3.21
+```
+
+```
+cp yapf-and-isort /path/to/project/.git/hooks/pre-commit
+chmod +x /path/to/project/.git/hooks/pre-commit
+```
--- install-jupyter-vim-binding.sh
+++ install-jupyter-vim-binding.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# refer to https://github.com/lambdalisue/jupyter-vim-binding
+mkdir -p $(jupyter --data-dir)/nbextensions
+cd $(jupyter --data-dir)/nbextensions
+git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
+jupyter nbextension enable vim_binding/vim_binding
+
 mkdir -p ~/.jupyter/custom
 ln -sf $PWD/jupyter/custom.js ~/.jupyter/custom
