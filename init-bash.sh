ceeb40ffc57f2d1f4a0bd323266b90c9d83b35fa
Fangjun Kuang <csukuangfj@gmail.com>
2020-03-29 14:42:20 +0800

    add config for bash.

--- init-bash.sh
+++ init-bash.sh
@@ -1,4 +1,38 @@
+
+if [ ! -e ~/.fangjun.sh ]; then
+  cat >> ~/.fangjun.sh << EOF
 set -o vi
 bind -x '"\C-l": clear'
 bind '"jk":vi-movement-mode'
 
+export EDITOR=vim
+EOF
+else
+  echo "skip creating ~/.fangjun.sh"
+fi
+
+if ! grep ".fangjun.sh" ~/.bashrc &> /dev/null; then
+  echo "source ~/.fangjun.sh" >> ~/.bashrc
+else
+  echo "skip appending ~/.bashrc"
+fi
+
+if ! grep "function add_path" ~/.fangjun.sh &> /dev/null; then
+  cat >> ~/.fangjun.sh << 'EOF'
+function add_path() {
+  export PATH="$1":${PATH}
+}
+EOF
+else
+  echo "skip add_path"
+fi
+
+if ! grep "function add_lib_path" ~/.fangjun.sh &> /dev/null; then
+  cat >> ~/.fangjun.sh << 'EOF'
+function add_lib_path() {
+  export LD_LIBRARY_PATH="$1":${LD_LIBRARY_PATH}
+}
+EOF
+else
+  echo "skip add_lib_path"
+fi
