dcda9816088b3aeb4b81b99d01142c79f05087ef
Fangjun Kuang <csukuangfj@gmail.com>
2020-11-24 10:50:04 +0800

    replace vim with nvim.
    deoplete does not need compilation!

---
+++ init-git.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global user.name "Fangjun Kuang"
+git config --global user.email "csukuangfj@gmail.com"
+git config --global diff.tool "vimdiff"
+git config --global core.editor "vim"
---
+++ init-nvim.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# sudo apt-get install neovim
+# sudo apt-get install python3-pip3
+# sudo apt-get install python3-pip
+# pip3 install --upgrade msgpack
+
+# wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
+# tar xf nvim-linux64.tar.gz
+#
+# add nvim-linux64/bin to PATH
+# cd nvim-linux64/bin; cp nvim vim
+
+mkdir -p ~/.config/nvim
+
+cat << EOF > ~/.config/nvim/init.vim
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath=&runtimepath
+source ~/.vimrc
+EOF
