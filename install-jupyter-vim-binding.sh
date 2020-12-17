efe1a051dbb0f7ffa97948d79e424858f1f52b7f
Fangjun Kuang <csukuangfj@gmail.com>
2020-12-17 12:43:09 +0800

    fix an error.

--- install-jupyter-vim-binding.sh
+++ install-jupyter-vim-binding.sh
@@ -2,9 +2,10 @@
 
 # refer to https://github.com/lambdalisue/jupyter-vim-binding
 mkdir -p $(jupyter --data-dir)/nbextensions
-cd $(jupyter --data-dir)/nbextensions
+pushd $(jupyter --data-dir)/nbextensions
 git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
 jupyter nbextension enable vim_binding/vim_binding
+popd
 
 mkdir -p ~/.jupyter/custom
 ln -sf $PWD/jupyter/custom.js ~/.jupyter/custom
