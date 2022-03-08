0350cf6fa11c8ab027135125580ef256816367b5
Fangjun Kuang <csukuangfj@gmail.com>
2022-03-08 12:01:39 +0800

    Some notes about pyenv

--- .my-pyenv.sh
+++ .my-pyenv.sh
@@ -7,3 +7,8 @@ if command -v pyenv 1>/dev/null 2>&1; then
 fi
 
 export PYTHON_CONFIGURE_OPTS="--enable-shared"
+
+# pyenv install --list
+# pyenv install 3.8.6
+# pyenv versions
+# pyenv global 3.8.6
